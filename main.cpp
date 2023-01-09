a10f8ec75a082d98bf93a662ee50cfa879a16e80
pair0 <dbzl159753@naver.com>
2023-01-10 00:19:18 +0900

    gilgil

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 
+using namespace std;
+
 void usage(){ //경고 메시지
     printf("syntax: airodump <interface>\n");
     printf("sample: airodump mon0\n");
@@ -23,6 +25,14 @@ int main(int argc, char** argv){
     struct Wireless* wrls;
     struct S_tag* s_tag;
     struct DS_tag* ds_tag;
+    char bssid_c[20];
+    char** packet_cp = (char**)calloc(300,sizeof(char*));
+    for(int i=0; i<20; i++){
+        packet_cp[i] = (char*)calloc(150,sizeof(char));
+    }
+    char essid_c[2];
+    int count = 0;
+    int beacon_count[100];
 
     char* dev = *(argv+1);
     char errbuf[PCAP_ERRBUF_SIZE];
@@ -33,14 +43,14 @@ int main(int argc, char** argv){
         return -1;
     }
 
-
-    puts("BSSID\t\t\tPWR  Beacons\tCH\tESSID\n");
-
     while(true) {
         struct pcap_pkthdr* header;
         const u_char* packet;
         const u_char* ESSID;
         const u_char* Supported_Rates;
+        char *essid_c_final = (char *)calloc(20, sizeof(char));
+
+        int cmp = -1;
         int res = pcap_next_ex(pcap, &header, &packet);
         if (res == 0) continue;
         if (res == -1 || res == -2) {
@@ -59,51 +69,48 @@ int main(int argc, char** argv){
             s_tag = (struct S_tag*)(packet + radiotap->length+sizeof(struct Beacon)+sizeof(struct Wireless)+wrls->ssid_len);
             Supported_Rates = packet + radiotap->length+sizeof(struct Beacon)+sizeof(struct Wireless)+wrls->ssid_len+sizeof(struct S_tag);
             const u_char* ex = packet + radiotap->length+sizeof(struct Beacon)+sizeof(struct Wireless)+wrls->ssid_len+sizeof(struct S_tag) + s_tag->s_len;
+            
             if(ex[0] == 0x03) ds_tag = (struct DS_tag*)(packet + radiotap->length+sizeof(struct Beacon)+sizeof(struct Wireless)+wrls->ssid_len+sizeof(struct S_tag)+s_tag->s_len);
             else ds_tag = (struct DS_tag*)(packet + radiotap->length+sizeof(struct Beacon)+sizeof(struct Wireless)+wrls->ssid_len+sizeof(struct S_tag)+s_tag->s_len+10);
             
-            //BSSID
-            printf("%02x:%02x:%02x:%02x:%02x:%02x\t", beacon->BSSID[0], beacon->BSSID[1], beacon->BSSID[2], beacon->BSSID[3], beacon->BSSID[4], beacon->BSSID[5]);
-            
-            //pwd
-            if(pcap_antenna(radiotap->Antenna_signal) >= 100) printf("-%d ", pcap_antenna(radiotap->Antenna_signal));
-            else if(pcap_antenna(radiotap->Antenna_signal) < 100 && pcap_antenna(radiotap->Antenna_signal) >= 10 ) printf("-%d  ", pcap_antenna(radiotap->Antenna_signal));
-            else if(pcap_antenna(radiotap->Antenna_signal) < 10) printf("-%d   ", pcap_antenna(radiotap->Antenna_signal));
-            
-            //beacon
-            printf("    %d\t", beacon->type);
-            
-            //channel
-            printf("%2x\t", ds_tag->current_channel);
+            sprintf(bssid_c,"%02x:%02x:%02x:%02x:%02x:%02x", beacon->BSSID[0], beacon->BSSID[1], beacon->BSSID[2], beacon->BSSID[3], beacon->BSSID[4], beacon->BSSID[5]);
 
-            //ssid
+            //essid 생성
             for(int i = 0; i<wrls->ssid_len; i++){
-                printf("%c", ESSID[i]);
+                sprintf(essid_c, "%c", ESSID[i]);
+                strcat(essid_c_final, essid_c);
             }
 
-            printf("\t");
-            printf("\n");
-        }
-        // else if(beacon->type == 0x0850){
-        //     wrls = (struct Wireless*)(packet+radiotap->length+sizeof(struct Beacon));
-        //     packet = packet + radiotap->length+sizeof(struct Beacon)+sizeof(struct Wireless);
-        //     printf("%02x:%02x:%02x:%02x:%02x:%02x\t", beacon->BSSID[0], beacon->BSSID[1], beacon->BSSID[2], beacon->BSSID[3], beacon->BSSID[4], beacon->BSSID[5]);
-        //     if(pcap_antenna(radiotap->Antenna_signal) >= 100) printf("-%d ", pcap_antenna(radiotap->Antenna_signal));
-        //     else if(pcap_antenna(radiotap->Antenna_signal) < 100 && pcap_antenna(radiotap->Antenna_signal) >= 10 ) printf("-%d  ", pcap_antenna(radiotap->Antenna_signal));
-        //     else if(pcap_antenna(radiotap->Antenna_signal) < 10) printf("-%d   ", pcap_antenna(radiotap->Antenna_signal));
-        //     printf("    %d\t", beacon->type);
-        //     printf("1\t");
+            for (int i=0; count-1 >= i; i++){
+                if (strstr(packet_cp[i], bssid_c) != NULL) {
+                    cmp = i;
+                    beacon_count[i] += 1;
+                    break;
+                }
+            }
+            if (cmp < 0) {
+                cmp = count;
+                beacon_count[count] = 1;
+                count += 1;
+            }
 
-        //     for(int i = 0; i<wrls->ssid_len; i++){
-        //                 printf("%c", packet[i]);
-        //     }
-        //     printf("\t");
-        //     printf("\n");
-        // }
+            //pwd
+            if(pcap_antenna(radiotap->Antenna_signal) >= 100) sprintf(packet_cp[cmp], "%s\t-%d \t%d\t%2x\t%s\n", bssid_c, pcap_antenna(radiotap->Antenna_signal), beacon_count[cmp], ds_tag->current_channel, essid_c_final);
+            else if(pcap_antenna(radiotap->Antenna_signal) < 100 && pcap_antenna(radiotap->Antenna_signal) >= 10 ) sprintf(packet_cp[cmp], "%s\t-%d  \t%d\t%2x\t%s\n", bssid_c, pcap_antenna(radiotap->Antenna_signal), beacon_count[cmp], ds_tag->current_channel, essid_c_final);
+            else if(pcap_antenna(radiotap->Antenna_signal) < 10) sprintf(packet_cp[cmp], "%s\t-%d   \t%d\t%2x\t%s\n", bssid_c, pcap_antenna(radiotap->Antenna_signal), beacon_count[cmp], ds_tag->current_channel, essid_c_final);
+            
+            system("clear");
+            puts("BSSID\t\t\tPWR  Beacons\tCH\tESSID\n");
+            for (int i=0; i<count; i++){
+                printf("%s", packet_cp[i]);
+            }
+        }
+        
         else{ //Probe Request일 시
             packet = packet + radiotap->length;
         }
     }
+    printf("\n\n%d\n\n", count);
     printf("\n\n");
 
     pcap_close(pcap);
